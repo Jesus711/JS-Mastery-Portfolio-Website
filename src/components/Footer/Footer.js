932140e1a477239dd2aa9359a894a1521375e736
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-07-03 18:07:52 -0700

    Finished the Footer section of the webpage. Uncommented Section components in index.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,44 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+
+        {/* <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:111-111-1111">111-111-1111</LinkItem>
+        </LinkColumn> */}
+
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:jruelasperez711@gmail.com">jruelasperez711@gmail.com</LinkItem>
+        </LinkColumn>
+
+      </LinkList>
+
+      <SocialIconsContainer>
+
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time.</Slogan>
+        </CompanyContainer>
+
+        <SocialIconsContainer style={{ flexDirection: "row", justifyContent: "flex-start" }} >
+          <SocialIcons href="https://github.com/Jesus711">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+
+          <SocialIcons href="https://www.linkedin.com/in/jesus-ruelas-20a319196/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+
+          {/* <SocialIcons href="https://instagram.com">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons> */}
+          
+        </SocialIconsContainer>
+
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
