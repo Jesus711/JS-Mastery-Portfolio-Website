eab159acc643ca9ae80538175bc338a87203b9e3
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-07-03 17:53:59 -0700

    Completed Hero section of the webpage

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section $row={true} $nopadding={true}>
+    <LeftSection>
+      <SectionTitle $main={true} $center={true}>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Hi, my name is Jesus Ruelas-Perez. I am a Computer Science Graduate aspiring to becoming a software engineer. I have experience with the following technologies:
+        Python, JavaScript, React, and SQL.
+      </SectionText>
+      {/* <Button onClick={() => window.location ="https://google.com"}>Learn More</Button> */}
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
