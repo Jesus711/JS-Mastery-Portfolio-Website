6988ea60efd6838d6d0a26be2002623db4972e5f
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-07-03 20:37:02 -0700

    Modified files to setup deployment of webpage to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
+  basePath: isProduction ? "/Portfolio-Website" : "",
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+  distDir: "dist",
 };
 
 export default nextConfig;
